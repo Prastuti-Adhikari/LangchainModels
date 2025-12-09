4ea4a40b33ec2faee6ee5fab89a01f6547beebd7
Prastuti-Adhikari <adhikariprastuti50@gmail.com>
2025-12-09 13:59:22 +0545

    huggingface local

---
+++ ChatModels/chatmodel_huggingface_local.py
@@ -0,0 +1,12 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+llm = HuggingFacePipeline.from_model_id(
+    model_id="meta-llama/Llama-3.2-1B-Instruct",
+    task="text-generation",
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
+)
+model = ChatHuggingFace(llm = llm)
+result = model.invoke("What is the tallest mountain in the world?")
+print(result.content)
